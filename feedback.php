f6442ea7a0845899a5151bac59128bd86a355502
dilconkihoro <dilcon.kihoro@strathmore.edu>
2024-07-11 10:44:03 +0300

    created nav.php

--- feedback.php
+++ feedback.php
@@ -1,4 +1,29 @@
-<?php include_once("templates/headerr.php");?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kihoro Safaris - Feedback</title>
+    <link rel="stylesheet" href="styles.css"> 
+</head>
+<body>
+    <header>
+        <img src="images/photox.png" alt="kihoro logo">
+        <h1>Welcome to Kihoro Safaris</h1>
+        <nav>
+            <ul>
+                <li><a href="index.html">Home</a></li>
+                <li><a href="about.html">About</a></li>
+                <li><a href="services.html">Services</a></li>
+                <li><a href="destinations.html">Destinations</a></li>
+                <li><a href="gallery.html">Gallery</a></li>
+                <li><a href="contact.html">Contact</a></li>
+                <li><a href="feedback.html">Feedback</a></li>
+                <li><a href="signup.html">Sign up</a></li>
+                <li><a href="signin.html">Sign In</a></li>
+            </ul>
+        </nav>
+    </header>
 
     <section class="feedback">
         <h2>Feedback</h2>
---
+++ templates/nav.php
@@ -0,0 +1,14 @@
+<nav>
+            <ul>
+                <li><a href="index.html">Home</a></li>
+                <li><a href="about.html">About</a></li>
+                <li><a href="services.html">Services</a></li>
+                <li><a href="destinations.html">Destinations</a></li>
+                <li><a href="gallery.html">Gallery</a></li>
+                <li><a href="contact.html">Contact</a></li>
+                <li><a href="feedback.html">Feedback</a></li>
+                <li><a href="signup.html">Sign up</a></li>
+                <li><a href="signin.html">Sign In</a></li>
+
+            </ul>
+        </nav>
